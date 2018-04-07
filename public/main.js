2d93ddabfed8883bdb1a38b1732d77f7736f4e03
jboudouris <32560102+jboudouris@users.noreply.github.com>
2018-04-07 14:32:21 -0500

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,17 +1,18 @@
 const PUBLIC_KEY = '6GcvQNz3vsFlJgGAcYc7Y4E6RRoylnTH';
 const BASE_URL = 'api.giphy.com/v1/gifs/';
-const ENDPOINT = 'search?';
+const ENDPOINT = 'random?';
 const LIMIT = 1;
 const RATING = 'pg';
 
 
-function getGifs() {
+function getGif() {
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://" + BASE_URL + ENDPOINT + "&api_key=" + PUBLIC_KEY + "&q=" + "funny", false);
+  xhr.open("GET", "https://" + BASE_URL + ENDPOINT + "&api_key=" + PUBLIC_KEY, false);
   xhr.send();
   var myArr = JSON.parse(xhr.responseText);
-  for (var i = 0; i < myArr.data.length; i++) {
-    var gifUrl = myArr.data[i].images.original.url
-    console.log(gifUrl);
-  }
+  console.log(myArr);
+  var gifUrl = myArr.data.images.original.url
+  var img = new Image();
+  img.src = gifUrl;
+  document.body.appendChild(img);
 }
